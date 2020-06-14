5dd78b1ae59c6b2eb3f7161d374c6e5d0ce30df7
Beatriz Rezener <beatrizrezener@gmail.com>
2020-06-14 02:42:25 -0300

    FeedRoute: Posts and Post

--- src/containers/Posts/Posts.jsx
+++ src/containers/Posts/Posts.jsx
@@ -4,6 +4,16 @@ import Post from '../../components/Post';
 
 const Posts = ({ posts, getUserHandler }) => (
   <div className="container" data-testid="posts">
+    <section className="feed">
+      { posts.length > 0 && posts.map(post =>
+          <Post
+            postInfo={post}
+            userInfo={getUserHandler(post.userId)}
+            key={post.id}
+          />
+        )
+      }
+    </section>
   </div>
 );
 
