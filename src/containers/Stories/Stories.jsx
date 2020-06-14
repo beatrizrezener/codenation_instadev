6bf66d127a98cf7e28c86d397811be3cda45b94a
Beatriz Rezener <beatrizrezener@gmail.com>
2020-06-14 02:06:19 -0300

    FeedRoute: Stories

--- src/containers/Stories/Stories.jsx
+++ src/containers/Stories/Stories.jsx
@@ -5,16 +5,50 @@ import Story from '../../components/Story';
 import './Stories.scss';
 
 const Stories = ({ stories, getUserHandler }) => {
+  const [showStory, toggleShowStory] = useState(false);
+  const [selectedStory, setSelectedStory] = useState({});
+  const [selectedUser, setSelectedUser] = useState({});
+  
+  const findStoryById = id => stories.find(story => story.id === id);
+
+  const handleStory = story => {
+    const foundStory = findStoryById(story.id);
+    const userData = getUserHandler(story.userId);
+
+    setSelectedUser(userData);
+    setSelectedStory(foundStory);
+    toggleShowStory(!showStory);
+  };
+
   return (
-    <React.Fragment data-testid="stories">
-      <section className="stories">
+    <React.Fragment>
+      <section className="stories" data-testid="stories">
         <div className="container">
+          {stories.map((story, index) => {
+            const userData = getUserHandler(story.userId);
+
+            return (
+              <button
+                key={story.id}
+                onClick={() => handleStory(story)}
+                className={`user__thumb ${index === 0 && 'user__thumb--hasNew'}`}
+              >
+                <div className="user__thumb__wrapper">
+                  <img src={userData.avatar} alt={userData.name} />
+                </div>
+              </button>
+            )})
+          }
         </div>
       </section>
 
-      {showStory && (
-        <Story />
-        )}
+      {showStory &&
+        <Story
+          story={selectedStory}
+          user={selectedUser}
+          handleClose={() => toggleShowStory(!showStory)}
+        />
+      }
     </React.Fragment>
   );
 };
--- src/routes/FeedRoute/FeedRoute.jsx
+++ src/routes/FeedRoute/FeedRoute.jsx
@@ -8,8 +8,33 @@ import Posts from '../../containers/Posts';
 import './FeedRoute.scss';
 
 const FeedRoute = () => {
+
+  const getUserPostById = postUserId => users.find(user => postUserId === user.id);
+  
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []); // Like componentDidMount and componentWillUnmount
+
+  const [stories, setStories] = useState([]);
+  useEffect(() => {
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories')
+      .then(res => res.json())
+      .then(data => {
+        setStories(data);
+      });
+  }, [users]); // Only useEffect again when users change. Like componentDidUpdate
+
   return (
     <div data-testid="feed-route">
+      {(users.length > 0 && stories.length > 0) && (
+        <Stories
+          stories={stories}
+          getUserHandler={getUserPostById}
+        />
+      )}
     </div>
   );
 };
